d241e8244d35fcf3534107adcafae1f5b97e79f7
i-rjt <rajatdagarcool001@gmail.com>
2025-02-04 19:32:53 +0530

    Fake Calculator

--- main.js
+++ main.js
@@ -1,60 +1,99 @@
 let on = true;
-let heading = document.getElementById("head");           
+let heading = document.getElementById("head");
 let btn = document.getElementById("flicker");
+let buffer = "";
+let operator = null;
+let total = 0;
 
-//Above 3 global variables defined to avoid redifing the variables inside multiple functions again & again
+//Above global variables defined to avoid redifing the variables inside multiple functions again & again
 
-
-function stopFlicker() {                            // This function will stop the flicker effect 
-    if (on == true) {
-        btn.addEventListener("click", () => {
-            heading.id = "heading";                        
+function flicker() {                            // This function will stop the flicker effect 
+    btn.addEventListener('click', () => {
+        if (on) {
+            heading.className = "heading";
             btn.innerText = "start flicker";
             on = false;
-            startFlicker();
-        });
-    }
-}
-
-stopFlicker();
-
-function startFlicker() {                              //Function will start the flicker effect                                           
-    if (on == false) {                                                               
-        btn.addEventListener("click", () => {
-            heading.id = "head";
+        } else {
+            heading.className = "head";
             btn.innerText = "stop flicker";
-            stopFlicker();
-        });
-        on = true;
+            on = true;
+        }
+    });
+}
+flicker();
+
+let calcBtn = document.querySelector(".calc-buttons").addEventListener('click', process);
+
+function process(event) {
+    let value = event.target.innerText;
+    if (isNaN(value)) {
+        symbol(value);
+    } else {
+        if (buffer == 0) buffer = "";
+        number(value);
+    }
+    display(buffer);
+}
+
+function number(num) {
+    total = parseInt(num);
+    buffer += num;
+    calculate(total);
+}
+
+function symbol(sym) {
+    let screen = document.querySelector(".screen");
+    switch (sym) {
+        case 'C':
+            screen.innerText = "0";
+            buffer = "0";
+            total = 0;
+            break;
+       case '←':
+            buffer--;
+            break;
+        case '÷':
+            buffer += "÷";
+            operator = "/";
+            break;
+        case '×':
+            buffer += "×";
+            operator = "*";
+            break;
+        case '−':
+            buffer += "−";
+            operator = "-"
+            break;
+        case '+':
+            buffer += "+";
+            operator = '+';
+            break;
+        case '=':
+            if ('=' == sym) {
+                operator = null;
+                buffer = total;
+            }
+            break;
+    }
+
+}
+
+function calculate(num) {
+    if (operator === "+") {
+        total += num;
+    }
+    else if (operator === "−") { total -= num; }
+    else if (operator === "×") { total *= num; }
+    else if (operator === "÷") {
+        if (num === 0) {
+            total = "Error";
+        } else {
+            total /= num;
+        }
     }
 }
 
-
-function process(){
-    let btn = document.querySelector(".calc-btn").addEventListener("click",(event)=>{
-        event.preventDefault();
-         console.log(event.target.innerText);
-    });
-    console.log(btn);
-}
-
-process();
-
-
-function number(){
-
-}
-
-function symbol(){
-
-}
-
-function calculate(num, sym){
-
-}
-
-
-
-function display(){
-
+function display(value) {
+    let screen = document.querySelector(".screen");
+    screen.innerText = value;
 }
